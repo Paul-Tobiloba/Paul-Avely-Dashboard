43a81b13cb627f17c7a47eecd6b26a065cf508e7
TobyCodes <twiseas9ice@gmail.com>
2020-05-30 07:22:41 +0100

    chore: fixed desktop view

---
+++ dashboard.js
@@ -0,0 +1,41 @@
+
+const CHART = document.getElementById('myChart').getContext('2d');
+console.log(CHART);
+let myChart = new Chart(CHART, {
+    type: 'line',
+    data: data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        datasets: [
+            {
+                label: 'My dataset',
+                fill: false,
+                lineTension: 0.5,
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderColor: "rgba(75,192,192,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,192,192,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+            }
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
